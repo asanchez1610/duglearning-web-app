995f6e8c18b935bee30903f2f8f7dd380a405160
Jose Arturo Sanchez Soto <asanchez.sys@gmail.com>
2020-10-30 04:06:17 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NzSelectSizeType } from 'ng-zorro-antd/select';
+import { NzButtonSize } from 'ng-zorro-antd/button';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  //Properties
   title = 'duglearning-web-app';
+  listDocsTypes: Array<{ label: string; value: string }> = [];
+  listSchools: Array<{ label: string; value: string }> = [];
+  docTypeSelected: String = 'DNI';
+  schoolsSelected: String = '';
+  sizeSelect: NzSelectSizeType = 'large';
+  sizeButton: NzButtonSize = 'large';
+  listCodes: Array<{ label: string; value: string }> = [];
+  listTagCodes = [];
+
+  ngOnInit(): void {
+
+    //Mocks tipo documento
+    const docs: Array<{ label: string; value: string }> = [];
+    docs.push({ label: 'DNI', value: 'DNI' });
+    docs.push({ label: 'RUC', value: 'RUC' });
+    docs.push({ label: 'CE', value: 'CE' });
+    docs.push({ label: 'PASSAPORTE', value: 'PASSAPORTE' });
+    this.listDocsTypes = docs;
+
+    //Mocks colegios
+    const schools: Array<{ label: string; value: string }> = [];
+    schools.push({ label: 'Colegio 01', value: '01' });
+    schools.push({ label: 'Colegio 02', value: '02' });
+    schools.push({ label: 'Colegio 03', value: '03' });
+    schools.push({ label: 'Colegio 04', value: '04' });
+    this.listSchools = schools;
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,78 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
+import { NZ_ICONS } from 'ng-zorro-antd/icon';
+import { IconDefinition } from '@ant-design/icons-angular';
+
+import * as AllIcons from '@ant-design/icons-angular/icons';
+
+import { DemoNgZorroAntdModule } from './ng-zorro-antd.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { es_ES } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+
+registerLocaleData(es);
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,FormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+    ReactiveFormsModule,
+    DemoNgZorroAntdModule,
+    BrowserAnimationsModule,
+    ScrollingModule,
+    DragDropModule
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ],
+  providers: [{ provide: NZ_I18N, useValue: es_ES }, { provide: NZ_ICONS, useValue: icons }]
 })
 export class AppModule { }
+
+
+/*
+
+
+
+
+
+
+
+
+@NgModule({
+  imports: [
+    BrowserModule,FormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+    ReactiveFormsModule,
+    DemoNgZorroAntdModule,
+    BrowserAnimationsModule,
+    ScrollingModule,
+    DragDropModule
+  ],
+  declarations: [ NzDemoGridOffsetComponent ],
+  bootstrap: [ NzDemoGridOffsetComponent ],
+  providers: [ { provide: NZ_I18N, useValue: en_US }, { provide: NZ_ICONS, useValue: icons } ]
+})
+export class AppModule { }
+*/
